4d2a80fedf31d9724da2d8b24e775557b5f1b59f
fx <scos_fx@fiberhome.com>
2017-10-23 14:24:53 +0800

    add welcome padge

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,29 +1,32 @@
 <template>
   <div class="hello">
-    <div class="logo">
+    <div class="logo mar_bot">
       <img src="../assets/fancyxi.png">
+    </div>
+    <div class="title mar_bot">
       <h1>{{ msg }}</h1>
     </div>
-      
-    <div class="description">
+    
+    <div class="title_description mar_bot">
       <h2>hi，你好！我是方希(@fancyxi).</h2>
     </div>
     
-    <div class="link">
-      <div class="blog-button" >
+    <div class="link_button mar_bot">
         <ul>
           <li><a href="#" target="_blank">博客</a></li>
           <li><a href="#" target="_blank">记事本</a></li>
           <li><a href="#" target="_blank">项目</a></li>
           <li><a href="#" target="_blank">关于</a></li>
         </ul>
-      </div>
-      <div>
-        <i class="fa fa-address-book" aria-hidden="true"></i>
-      </div>
     </div>
-    
-    
+    <div class="link_icon mar_bot">
+      <ul>
+        <li><a href="#" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+        <li><a href="#" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+        <li><a href="#" target="_blank"><i class="fa fa-weixin" aria-hidden="true"></i></a></li>
+        <li><a href="#" target="_blank"><i class="fa fa-rss" aria-hidden="true"></i></a></li>  
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -56,7 +59,7 @@ li {
 }
 
 a {
-  color: #42b983;
+  color: #000;
   text-decoration:none;
 }
 .logo img{
@@ -66,7 +69,8 @@ a {
   border: 3px solid #FFF;
   box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.3);
 }
-.blog-button a{
+.link_button a{
+    color: #42b983;
     padding: 10px 20px;
     border: 1px solid #42b983;
     border-radius: 20px;
@@ -75,5 +79,26 @@ a {
     letter-spacing: 1px;
     text-shadow: none;
     -webkit-font-smoothing: antialiased;
+}
+.hello {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction:column;
+    flex-wrap: nowrap;
+    align-items:center;
+    justify-content: center;
+}
+.fa {
+  font-size: 28px;  
+}
+.mar_bot {
+  margin-bottom: 20px;
+}
+.link_button{
+  margin-top: 20px;
+}
+.link_icon{
+  margin-top: 20px;
 }
 </style>
